8d95ce7b72a14dd0cd428a3acb718c7be6b54aac
NathanDenisse <80519951+NathanDenisse@users.noreply.github.com>
2021-10-26 16:42:58 -0700

    Add files via upload

---
+++ Matter.js V0/sketch.js
@@ -0,0 +1,16 @@
+// créer des alias pour les modules
+
+function setup() {
+  createCanvas(windowWidth, windowHeight * 0.8);
+  //Créer un moteur pour le monde
+
+  //Créer le monde
+
+  //Créer des objets
+
+  // mettre le corps dans le monde
+}
+
+function draw() {
+  background(200);
+}
